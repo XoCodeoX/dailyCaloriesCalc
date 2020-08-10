36bd87e3d2922d9e847eb19564a8657f3b0b19b5
Mateusz XoCodeXo <mateusz_skibicki@o2.pl>
2020-08-10 09:11:10 +0200

    Create content of ProductRow Component

--- src/components/ProductRow.js
+++ src/components/ProductRow.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 class ProductRow extends Component {
     render() {
         return (
-            <div>RandomText</div>
+            this.props.arrayOfProducts.map(element => !element.done ?
+                <tr key={element.id}>
+                    <td>{element.productName}</td>
+                    <td>{element.calories}</td>
+                    <td>{element.quantity}</td>
+                    <td>
+                        <input type="checkbox" checked={element.done} onChange={() => this.props.callback(element)} />
+                    </td>
+                </tr> : null)
+
         )
     }
 }
