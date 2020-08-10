2f59d64557d4d898ae4411c3dd31c9996d997d41
Mateusz XoCodeXo <mateusz_skibicki@o2.pl>
2020-08-10 08:49:20 +0200

    Create content for FormElements and integrate with App.js - main component

--- src/components/FormElements.js
+++ src/components/FormElements.js
@@ -3,7 +3,30 @@ import React, { Component } from 'react';
 class FormElements extends Component {
     render() {
         return (
-            <div>RandomText</div>
+            <div className="my-1">
+                <input type="text"
+                    name="prodName"
+                    className="form-control mb-3"
+                    placeholder="Podaj nazwę produktu"
+                    onChange={this.props.callback}
+                    value={this.props.productName} />
+
+                <input type="text"
+                    name="prodCalories"
+                    className="form-control mb-3 "
+                    placeholder="Podaj kalorie"
+                    onChange={this.props.callback}
+                    value={this.props.productCalories} />
+
+                <input type="text"
+                    name="prodQuantity"
+                    className="form-control mb-3"
+                    placeholder="Podaj liczbę sztuk"
+                    onChange={this.props.callback}
+                    value={this.props.productQuantity} />
+
+                <button onClick={this.props.addProduct} className="btn btn-primary mt-1 mb-4">Dodaj do listy produktów</button>
+            </div>
         )
     }
 }
